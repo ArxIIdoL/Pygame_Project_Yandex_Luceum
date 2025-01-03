4c6cee9287e3b26f73bf2c47f413da766b2c0ee1
F3D0RP <fedor090808@gmail.com>
2025-01-03 20:54:54 +0300

    Class AnimatedSprite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+
 class Ship:
     def __init__(self, x, y):
         self.x = x
@@ -23,6 +24,7 @@ class Ship:
             self.bullets.append(bullet)
             self.last_shot_time = current_time
 
+
 class Bullet:
     def __init__(self, x, y):
         self.x = x
@@ -32,6 +34,7 @@ class Bullet:
 
     def move(self):
         self.y -= self.bullet_speed
+
 
 def main():
     pygame.init()
@@ -73,6 +76,30 @@ def main():
         clock.tick(240)
 
     pygame.quit()
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__()
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
 
 if __name__ == "__main__":
     main()
